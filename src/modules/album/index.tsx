a28d5779cc240d1ec573e3048c9a1698a22470ae
khusbo-ak <khusbo.alam.ak@gmail.com>
2025-10-29 14:12:13 +0600

    API fetch, use state, use effect

---
+++ src/modules/album/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const Album = () => {
+  const [albums, setAlbums] = useState([]);
+  console.log("Albums:", albums);
+  async function getData() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/photos"
+      );
+      const data = await response.json();
+      console.log(data);
+      setAlbums(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="bg-white text-black min-h-screen ">
+      {/* <button onClick={getData}>View Albums</button> */}
+      {albums.length > 0 && (
+        <div className="grid grid-cols-4 gap-4 p-4 bg-blue-600">
+          {albums.map((album: any) => (
+            <div className="bg-slate-800 text-white border" key={album.id}>
+              <p>{album.title}</p>
+              <img src={album.thumbnailUrl} alt={album.title} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Album;
